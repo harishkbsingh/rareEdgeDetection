6592da654eb0187aeb05a741eb3fc595b038015e
Angel Cambero <50842946+angelc-lw@users.noreply.github.com>
2019-06-24 16:27:53 -0700

    Version 1.2
    In this version we updated the following:
    1. The factor to calculate the number of times in terms of connection to classify an edge as rare is determined dynamically by using a polynomial regression function computed using least square based on the existing date.
    2. Refactored the whole code, removing unnecessary code and making it more readable.

--- algo/main.py
+++ algo/main.py
@@ -1,17 +1,11 @@
 class Algorithm():
     
-    # Constants 
+    # Variables
+    _trackAllSpikesValue = []
+    _trackAllSpikesPosition = []
     trainingTime = 30 * 24
-    timeLast60days = 60 * 24
-    timeLast30days = 30 * 24
-    timeLast15days = 15 * 24
-    timeLast5days  = 5 * 24
-    timeLast1days  = 24
-    timeLast5hours = 5
     
     # Results       
-    trackAllSpikesValue = []
-    trackAllSpikesPosition = []
     rareEdgesTrack = {
                         'positions': [], 
                         'values': [], 
@@ -25,28 +19,27 @@ class Algorithm():
     
     # Training first 30 days
     def learnFirst30Days(self):
-        
         self.timeSerieHours = len(self.ts)
         assert self.timeSerieHours > 30, "not enough data"
-        
         self.rareEdgesTrack['positions'].append(self.trainingTime - 1)
         self.rareEdgesTrack['values'].append(np.amax(self.ts[:self.trainingTime]))
         self.rareEdgesTrack['reported'].append(False)
-        self.trackAllSpikesValue.append(np.amax(self.ts[:self.trainingTime]))
-        self.trackAllSpikesPosition.append(self.trainingTime - 1)
+        self._trackAllSpikesValue.append(np.amax(self.ts[:self.trainingTime]))
+        self._trackAllSpikesPosition.append(self.trainingTime - 1)
         return
-        
+    
+    # Polynomial regressions (polynomial least squares fittings).
+    def decayFunction(self, TSLRE):
+        return (-0.04448 * (TSLRE/24)) + 3.5 if TSLRE/24 < 60 else float('inf') # Generalize better than order 2
+#       return (0.0006430209079 * (TSLRE/24)**2) - (0.1041791699 *(TSLRE/24)) + 4.872672204 if TSLRE/24 < 60 else float('inf')
+     
+    
     # Algorithm    
     def rareEdge_detection(self):
 
-        # Initializations
-        flag = False
-        spike = False
-        
         #State Variables (Online Version)
         state_mean = 0
         
-
         # Training & Detection
         for i in range(self.trainingTime, self.timeSerieHours):
 
@@ -63,17 +56,12 @@ class Algorithm():
             flag = False
             toReport = False
             
-        
-            # Heuristics
-            if (TSLRE > self.timeLast60days and tick > np.mean(self.rareEdgesTrack['values']) * 0.50) or \
-               (TSLRE > self.timeLast30days and TSLRE < self.timeLast60days and lastRE * 1.50 < tick) or \
-               (TSLRE > self.timeLast15days and TSLRE < self.timeLast30days and lastRE * 2 < tick) or \
-               (TSLRE > self.timeLast5days and TSLRE < self.timeLast15days and lastRE * 3 < tick) or \
-               (TSLRE > self.timeLast1days and TSLRE < self.timeLast5days and lastRE * 5 < tick):
-                self.rareEdgesTrack['values'].append(tick)
-                self.rareEdgesTrack['positions'].append(i)
+            # Candidate to be a rare edge
+            if self.decayFunction(TSLRE) * lastRE < tick:
                 toReport = True
-            elif tick > lastRE:
+            
+            # Tracking rare edges
+            if toReport or tick > lastRE:
                 self.rareEdgesTrack['values'].append(tick)
                 self.rareEdgesTrack['positions'].append(i)
         
@@ -81,15 +69,15 @@ class Algorithm():
             if toReport:
                 
                 # Learning (24 hours after an rare edge, these are tracked but no re-reported)
-                if (TSLRE < self.timeLast1days and lastRE < tick):
+                if (TSLRE < 24 and lastRE < tick):
                     toReport = False
 
                 # Rare edges should be 3x greater than the mean of the spikes after the last rare edge was detected
-                if tick < (np.mean([value for posValue, value in enumerate(self.trackAllSpikesValue) if self.trackAllSpikesPosition[posValue] < TSLRE])) * 3:
+                if tick < (np.mean([value for posValue, value in enumerate(self._trackAllSpikesValue) if self._trackAllSpikesPosition[posValue] < TSLRE])) * 3:
                     toReport = False
 
                 # Rare edges should be 1.5x greater than greatest spike after last last rare edge 
-                theValues = [self.trackAllSpikesValue[index] for index, position in enumerate(self.trackAllSpikesPosition) if position < TSLRE]
+                theValues = [self._trackAllSpikesValue[index] for index, position in enumerate(self._trackAllSpikesPosition) if position < TSLRE]
                 if len(theValues) > 0 and tick < np.max(theValues) * 1.50:
                     toReport = False
                     
@@ -98,16 +86,15 @@ class Algorithm():
             
             # A spike is at least 75% of the last of rare edge
             if tick > lastRE * 0.75:
-                self.trackAllSpikesValue.append(tick)
-                self.trackAllSpikesPosition.append(i)
+                self._trackAllSpikesValue.append(tick)
+                self._trackAllSpikesPosition.append(i)
             
             
-        return self.rareEdgesTrack    
+        return self.rareEdgesTrack   
     
     
 # Run Algo    
 algoInstance = Algorithm(time_series, 0.5) #Hyperparemeter 0-1
-# algoInstance = Algorithm(time_series)
 algoInstance.learnFirst30Days()
 rareEdgesTrack = algoInstance.rareEdge_detection()
 
