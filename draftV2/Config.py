85e8dddeee168a42e295da65c35c8074b8b84eee
Angel Cambero <50842946+angelc-lw@users.noreply.github.com>
2019-07-25 09:37:01 -0700

    Update Config.py

--- draftV2/Config.py
+++ draftV2/Config.py
@@ -1,14 +1,14 @@
 # # PREPROD
 loginInfo = ['dev_angel', 'lwdev', 'DEV_TEST',
              'PREPROD_CDB_SNOWFLAKE_E6FB97EF9C0EFB48D1AA880139F22303', 'GRAPH_INTERNAL',
-             'L@cework809!']
+             '************']
 # monitoredEdgesTable = 'angel_test_monitored_edges'
 #
 
 #  SNOWFLAKE
 # loginInfo = ['dev_angel', 'lwdev', 'DEV_TEST',
 #              'PREPROD_CDB_PREPROD_9593B140CA75F1259DD584236657F2A996A6FA2A14B603FF', 'GRAPH_INTERNAL',
-#              'L@cework809!']
+#              '************']
 monitoredEdgesTable = 'angel_test_monitored_edges_snowflake'
 
 
