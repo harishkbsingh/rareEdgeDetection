dd0d71161d169db6728908248648ae20547e7464
Angel Cambero <50842946+angelc-lw@users.noreply.github.com>
2019-07-25 11:08:23 -0700

    Updating algo for concurrent edges analysis

--- draftV2/Algorithm.py
+++ draftV2/Algorithm.py
@@ -2,33 +2,21 @@ import math
 import numpy as np
 from collections import defaultdict
 
+time_series_expectedValue = defaultdict(float)
 time_series_expected = defaultdict(list)
-time_series_counter = defaultdict(int)
+time_series_currentHour = defaultdict(int)
+time_series_lastMaxValue = defaultdict(int)
+time_series_lastMaxPosition = defaultdict(int)
 decayFunction_values = []
 
+time_series_lastReportedRareEdge = defaultdict(int)
+time_series_lastRareEdgePosition = defaultdict(int)
 
 class Algorithm():
-
-    # Initialized with the Timeserie
-    def __init__(self):
-        self.hyperparam = None
-        self.reset()
-        #         time_series_expected = []
-        self.cumulativeValues = []
-        self.previousExpected = 0
 
     # Polynomial regressions (polynomial least squares fittings).
     def decayFunction(self, TSLRE):
         return (-0.04448 * (TSLRE / 24)) + 3.6  # Generalize better than order 2
-
-    def secondDecayFunction(self, lastTick, value):
-        return abs(lastTick - value)
-
-    def reset(s):
-        s.meanSinceLastRareEdge = 0
-        s.stdSinceLastRareEdge = 0
-        s.timeSinceLastRareEdge = 0
-        s.expectedValue = 0
 
     currentHour = -1  # 0 in first iteration
     trainingTime = 30 * 24
@@ -41,61 +29,64 @@ class Algorithm():
 
     def feed(s, key, tick):
 
-        s.cumulativeValues.append(tick)
+        # s.cumulativeValues.append(tick)
         avg = 0  # np.mean(s.cumulativeValues[-24:]) --- uncommented
         std = 0  # np.std(s.cumulativeValues[-200:]) --- uncommented
 
         # Training first 30 days
-        time_series_counter[key] = time_series_counter[key] + 1
-        s.currentHour = time_series_counter[key]
+        time_series_currentHour[key] = time_series_currentHour[key] + 1
+        s.currentHour = time_series_currentHour[key]
 
         if s.currentHour < s.trainingTime:
-            s.lastMaxValue = max(s.lastMaxValue, tick)
+            time_series_lastMaxValue[key] = max(time_series_lastMaxValue[key], tick)
+            # s.lastMaxValue = time_series_maxValues[key]
             time_series_expected[key].append(0)  # UI
-            # print('Training ', s.currentHour)
+            # print('Training ', key, s.currentHour)
             return
 
         elif s.currentHour == s.trainingTime:
 
-            s.lastMaxValue = max(s.lastMaxValue, tick)
-            s.lastMaxValuePosition = s.trainingTime
-            s.expectedValue = (s.decayFunction(1) * s.lastMaxValue)  ## working ?
-            s.previousExpected = s.expectedValue
-            time_series_expected[key].append(s.expectedValue)  # UI
-            # print("*************************** Key: ", key)
+            time_series_lastMaxValue[key] = max(time_series_lastMaxValue[key], tick)
+            time_series_lastMaxPosition[key] = s.trainingTime
+            time_series_expectedValue[key] = (s.decayFunction(1) * time_series_lastMaxValue[key])  ## working ?
+            time_series_expected[key].append(time_series_expectedValue[key])  # UI
             print('Training Completed!!')
             return
 
         # Time Since Last Rare Edge
-        TSLMV = s.currentHour - s.lastMaxValuePosition
-        TSLRE = s.currentHour - s.lastReportedRareEdge
-        # print('Analyzing ', s.currentHour)
+        TSLMV = s.currentHour - time_series_lastMaxPosition[key]
+        TSLRE = s.currentHour - time_series_lastReportedRareEdge[key]
 
         if s.currentHour == 1000:
             print("One Thousand")
+            print('Analyzing ', key, s.currentHour)
         if s.currentHour == 2000:
             print("Two Thousand")
+            print('Analyzing ', key, s.currentHour)
         if s.currentHour == 3000:
             print("Three Thousand")
+            print('Analyzing ', key, s.currentHour)
         if s.currentHour == 4000:
             print("Four Thousand")
+            print('Analyzing ', key, s.currentHour)
         if s.currentHour == 5000:
             print("Five Thousand")
+            print('Analyzing ', key, s.currentHour)
 
 
-        if tick > s.expectedValue:
+        if tick > time_series_expectedValue[key]:
             if (TSLRE > 24):
                 print("*************************** ALERT: ", key)
                 print("*************************** Hour: ", s.currentHour, " ----> Value: ", int(tick))
-                lastReportedRareEdge = s.currentHour
+                time_series_lastReportedRareEdge[key] = s.currentHour
 
-        if (s.decayFunction(TSLMV) * s.lastMaxValue) < s.decayFunction(1) * tick:
-            s.expectedValue = s.decayFunction(1) * tick
-            s.expectedValue = s.expectedValue + std
-            s.lastMaxValuePosition = s.currentHour
-            s.lastMaxValue = tick
+        if (s.decayFunction(TSLMV) * time_series_lastMaxValue[key]) < s.decayFunction(1) * tick:
+            time_series_expectedValue[key] = s.decayFunction(1) * tick
+            time_series_expectedValue[key] = time_series_expectedValue[key] + std
+            time_series_lastMaxPosition[key] = s.currentHour
+            time_series_lastMaxValue[key] = tick
         else:
-            s.expectedValue = s.decayFunction(TSLMV) * s.lastMaxValue
-            s.expectedValue = s.expectedValue + std
+            time_series_expectedValue[key] = s.decayFunction(TSLMV) * time_series_lastMaxValue[key]
+            time_series_expectedValue[key] = time_series_expectedValue[key] + std
 
-        time_series_expected[key].append(s.expectedValue)
+        time_series_expected[key].append(time_series_expectedValue[key])
