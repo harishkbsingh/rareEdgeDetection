8452bc7a0dc6127065f00d0800ce95a5f9495bc8
Angel Cambero <50842946+angelc-lw@users.noreply.github.com>
2019-08-01 15:49:12 -0700

    Update Main.py

--- draftV3/Main.py
+++ draftV3/Main.py
@@ -16,7 +16,7 @@ def runMonthsSimulation():
     print('*************************** Analysis Completed')
 
 '''
-Algorithm ran for each edge (source, destination) in the current hour 
+Instanced called for each edge (source, destination) with connections in the current hour 
 Input: algorithm instance (instance) and current date (date)
 Output: None
 '''
